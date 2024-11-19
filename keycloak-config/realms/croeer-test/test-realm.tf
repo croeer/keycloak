5d967d7b3e540bcb39ca4c4c4f0e2b0eac39f63c
croeer <christian@roeer.info>
2024-11-19 20:42:02 +0100

    add weddash client

--- keycloak-config/realms/croeer-test/test-realm.tf
+++ keycloak-config/realms/croeer-test/test-realm.tf
@@ -21,6 +21,26 @@ resource "keycloak_openid_client" "plungestreak_openid_client" {
 
 }
 
+
+resource "keycloak_openid_client" "weddash_openid_client" {
+  realm_id  = keycloak_realm.croeer_test.id
+  client_id = "weddash-client"
+
+  name    = "weddash"
+  enabled = true
+
+  access_type = "PUBLIC"
+  valid_redirect_uris = [
+    "http://localhost*",
+    "https://hochzeit.roeer.net/*"
+  ]
+  web_origins           = ["*"]
+  standard_flow_enabled = true
+
+  login_theme = "keycloak"
+
+}
+
 resource "keycloak_user" "chris_user_with_initial_password" {
   realm_id = keycloak_realm.croeer_test.id
   username = "chris"
@@ -28,10 +48,25 @@ resource "keycloak_user" "chris_user_with_initial_password" {
 
   email      = "chris@ku0.de"
   first_name = "Chris"
-  last_name  = "Kross"
+  last_name  = "Test"
 
   initial_password {
     value     = "chris"
+    temporary = false
+  }
+}
+
+resource "keycloak_user" "sandra_user_with_initial_password" {
+  realm_id = keycloak_realm.croeer_test.id
+  username = "sandra"
+  enabled  = true
+
+  email      = "sandra@ku0.de"
+  first_name = "Sandra"
+  last_name  = "Test"
+
+  initial_password {
+    value     = "sandra"
     temporary = false
   }
 }
